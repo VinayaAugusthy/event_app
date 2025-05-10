e4b514a560285df8b031f4b20e2eeeca28fbf076
Vinaya <vinayaaugusthy@gmail.com>
2025-05-10 22:13:19 +0530

    Fixed usermodel

--- lib/src/features/auth/data/model/user_model.dart
+++ lib/src/features/auth/data/model/user_model.dart
@@ -1,47 +1,4 @@
-import 'dart:convert';
-
 import 'package:cloud_firestore/cloud_firestore.dart';
-
-// class UserModel {
-//   String? userId;
-//   String? email;
-//   String? password;
-//   UserModel({this.userId, this.email, this.password});
-
-//   Map<String, dynamic> toMap() {
-//     final result = <String, dynamic>{};
-
-//     if (userId != null) {
-//       result.addAll({'userId': userId});
-//     }
-//     if (email != null) {
-//       result.addAll({'email': email});
-//     }
-//     if (password != null) {
-//       result.addAll({'password': password});
-//     }
-
-//     return result;
-//   }
-
-//   factory UserModel.fromMap(Map<String, dynamic> map) {
-//     return UserModel(
-//       userId: map['userId'],
-//       email: map['email'],
-//       password: map['password'],
-//     );
-//   }
-
-//   String toJson() => json.encode(toMap());
-
-//   factory UserModel.fromJson(String source) =>
-//       UserModel.fromMap(json.decode(source));
-//   static UserModel fromSnap(DocumentSnapshot snap) {
-//     var snapshot = snap.data() as Map<String, dynamic>;
-
-//     return UserModel(email: snapshot['email'], userId: snapshot['userId']);
-//   }
-// }
 
 class UserModel {
   final String userId;
